9f74861cfbd5a1ec90c860cbf188a3af8939f857
va5015 <va5015@naver.com>
2019-03-03 11:41:39 +0900

    파이썬스터디 2회차

---
+++ test022-24.py
@@ -0,0 +1,21 @@
+a = True
+b = False
+print(a==1)
+print(b!=0)
+
+x = 1; y = 2
+str1 = 'abc'; str2 ='python'
+print(x==y)
+print(x !=y)
+print(str1 == str2)
+print(str2=='python')
+print(str1<str2)
+
+bool1 =True; bool2 = False; bool3 = True; bool4 = False
+print(bool1 and bool2)
+print(bool1 and bool3)
+print(bool2 or bool3)
+print(bool2 or bool4)
+print(not bool1)
+print(not bool2)
+
---
+++ test026.py
@@ -0,0 +1,6 @@
+strdata = 'abcde'
+listdata = [1,[2,3],'안녕']
+tupledata = (100,200,300)
+
+a = 1
+print(listdata[1][-1])
